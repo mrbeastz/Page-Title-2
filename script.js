68b0842fc24a3b7559eca6912b550d785f298d71
mrbeastz <43611204+mrbeastz@users.noreply.github.com>
2019-03-05 09:36:29 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,33 @@
+$("button").click(function(){
+ var comment= $(".yourName").val();
+ 
+ $('.message').append("<p>" + comment + "<p>");
+ 
+ });
+
+
+
+
+
+
+
+$("button").click(function(){
+ var comment= $(".foodName").val();
+ 
+ $('.message').append("<p>" + comment + "<p>");
+ 
+});
+
+
+
+
+
+
+$("button").click(function(){
+ var comment= $(".amountEaten").val();
+ 
+ $('.message').append("<p>" + comment + "<p>");
+ 
+ 
+ 
+});
